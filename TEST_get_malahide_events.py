0f25fd5e86e020f02896d42fe8b1c01e89704939
Logan <lczernel@gmail.com>
2018-04-02 21:09:37 +0100

    added future events
    One screen displays tonight's events, the future events screen displays multiple upcoming events

--- TEST_get_malahide_events.py
+++ TEST_get_malahide_events.py
@@ -14,7 +14,10 @@ def main():
     """
     Use the above functions to print to output.txt the relevant(by location and time) events and their details
 
-    :return: dictionaries full of information on upcoming events
+    :return tonights_events: dictionaries full of information on tonights events
+    :rtype: list
+
+    :return all_upcoming_events: dictionaries full of information on all foreseeable events
     :rtype: list
     """
 
@@ -24,35 +27,38 @@ def main():
     gibneys_events = get_gibneys_events()
     duffys_events = get_duffys_events()
     old_school_house_events = get_old_school_house_events()
+    tonights_events = []
     all_upcoming_events = []
 
     for counter in range(len(gibneys_events)+2):
 
         try:
+            all_upcoming_events.append(gibneys_events.get(str(counter)))
             if weekday in gibneys_events.get(str(counter)).get('Date').lower():
-                all_upcoming_events.append(gibneys_events.get(str(counter)))
+                tonights_events.append(gibneys_events.get(str(counter)))
         except AttributeError:
             continue
 
     for counter in range(len(duffys_events)+2):
 
         try:
-
+            all_upcoming_events.append(duffys_events.get(str(counter)))
             if weekday == duffys_events.get(str(counter)).get('Date').lower()[0:-2]:
                 if now.day == int(duffys_events.get(str(counter)).get('Date').lower()[-2:]):
-                    all_upcoming_events.append(duffys_events.get(str(counter)))
+                    tonights_events.append(duffys_events.get(str(counter)))
         except AttributeError:
             continue
 
     for counter in range(len(old_school_house_events)+2):
 
         try:
+            all_upcoming_events.append(old_school_house_events.get(str(counter)))
             if old_school_house_events.get(str(counter)).get('Date').lower() == weekday + f" {now.day}":
-                all_upcoming_events.append(old_school_house_events.get(str(counter)))
+                tonights_events.append(old_school_house_events.get(str(counter)))
         except AttributeError:
             continue
 
-    return all_upcoming_events
+    return [tonights_events, all_upcoming_events]
 
 
 if __name__ == '__main__':
--- kivy_app.py
+++ kivy_app.py
@@ -20,24 +20,73 @@ class MainScreen(Screen):
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
 
-        my_layout = AnchorLayout(anchor_x='center', anchor_y='top', padding=[5, 5, 5, 5])
-        new_button = Button(text='Find The Beat')
-        new_button.background_color = (0.89, 0.49, 0.1, 1)
-        new_button.size_hint_max = (110, 50)
-        new_button.border = [20, 20, 20, 20]
-        new_button.bind(on_press=self.changer)
-        my_layout.add_widget(new_button)
-        self.add_widget(my_layout)
+        my_layout_one = AnchorLayout(anchor_x='center', anchor_y='top', padding=[5, 5, 5, 5])
+        my_layout_two = AnchorLayout(anchor_x='center', anchor_y='bottom', padding=[5, 5, 5, 5])
 
-    def changer(self, *args):
+        to_results_button = Button(text='Tonight\'s Beat')
+        to_results_button.background_color = (0.89, 0.49, 0.1, 1)
+        to_results_button.size_hint_max = (110, 50)
+        to_results_button.border = [20, 20, 20, 20]
+        to_results_button.bind(on_press=self.changer_one)
+
+        to_fresults_button = Button(text='Future\'s Beat')
+        to_fresults_button.background_color = (0.89, 0.49, 0.1, 1)
+        to_fresults_button.size_hint_max = (110, 50)
+        to_fresults_button.border = [20, 20, 20, 20]
+        to_fresults_button.bind(on_press=self.changer_two)
+
+        my_layout_two.add_widget(to_fresults_button)
+        my_layout_one.add_widget(to_results_button)
+        self.add_widget(my_layout_one)
+        self.add_widget(my_layout_two)
+
+    def changer_one(self, *args):
         self.manager.current = 'results'
+        self.manager.transition.direction = 'down'
+
+    def changer_two(self, *args):
+        self.manager.current = 'fresults'
         self.manager.transition.direction = 'up'
 
 
-class ResultsScreen(Screen):
+class TonightResultsScreen(Screen):
 
     def __init__(self, **kwargs):
-        super(ResultsScreen, self).__init__(**kwargs)
+        super(TonightResultsScreen, self).__init__(**kwargs)
+
+        my_layout = AnchorLayout(anchor_x='center', anchor_y='bottom', padding=[5, 5, 5, 5])
+        new_button = Button(text='Back To Home')
+        new_button.background_color = (0.89, 0.49, 0.1, 1)
+        new_button.size_hint_max = (110, 50)
+        new_button.bind(on_press=self.changer)
+        my_layout.add_widget(new_button)
+
+        results_layout = BoxLayout(orientation='vertical', spacing=5)
+        output_string = ''
+
+        if len(main()[0]) >= 1:
+            for local_event in main()[0]:
+                if local_event is not None:
+                    for event_property in local_event:
+                        output_string += "{}: {} \n".format(event_property, local_event.get(event_property))
+        else:
+            output_string = 'I am sorry, \n I could not find any events today!'
+
+        results_label = Label(text=output_string, font_size=10)
+        results_layout.add_widget(results_label)
+
+        self.add_widget(results_layout)
+        self.add_widget(my_layout)
+
+    def changer(self, *args):
+        self.manager.current = 'main'
+        self.manager.transition.direction = 'up'
+
+
+class FutureResultsScreen(Screen):
+
+    def __init__(self, **kwargs):
+        super(FutureResultsScreen, self).__init__(**kwargs)
 
         my_layout = AnchorLayout(anchor_x='center', anchor_y='top', padding=[5, 5, 5, 5])
         new_button = Button(text='Back To Home')
@@ -49,14 +98,15 @@ class ResultsScreen(Screen):
         results_layout = BoxLayout(orientation='vertical', spacing=5)
         output_string = ''
 
-        if len(main()) >= 1:
-            for local_event in main():
-                for event_property in local_event:
-                    output_string += "{}: {} \n".format(event_property, local_event.get(event_property))
+        if len(main()[0]) >= 1:
+            for local_event in main()[1]:
+                if local_event is not None and len(output_string) < 400:
+                    for event_property in local_event:
+                        output_string += "{}: {} \n".format(event_property, local_event.get(event_property))
         else:
             output_string = 'I am sorry, \n I could not find any events today!'
 
-        results_label = Label(text=output_string)
+        results_label = Label(text=output_string, font_size=10)
         results_layout.add_widget(results_label)
 
         self.add_widget(results_layout)
@@ -79,10 +129,12 @@ class MyApp(App):
     def build(self):
         new_screenmanager = ScreenManagement()
         first_screen = MainScreen(name='main')
-        second_screen = ResultsScreen(name='results')
+        second_screen = TonightResultsScreen(name='results')
+        third_screen = FutureResultsScreen(name='fresults')
         new_screenmanager.add_widget(first_screen)
         new_screenmanager.add_widget(second_screen)
-        self.title = 'Follow The Beat'
+        new_screenmanager.add_widget(third_screen)
+        self.title = 'Find The Beat'
 
         self.root = root = new_screenmanager.current_screen
         root.bind(size=self._update_rect, pos=self._update_rect)
